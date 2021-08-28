0eeb67a6caf1b47fb9039ca6a9f0143f74267b9f
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-08-28 00:09:04 -0300

    exercicios

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/bloco-04-03/exercicio1.js
@@ -0,0 +1,17 @@
+ let m = 1
+
+ for (m; m<=5; m=m+1){
+     console.log("*****")
+ }
+
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/bloco-04-03/exercicio3.js
@@ -0,0 +1,9 @@
+let n =5;
+let simbolo = "*";
+let linha ="";
+let espaço = "_"
+
+for (let index = 0; index < n; index+=1){
+    linha = linha + espaço;
+    console.log(linha)
+} 
