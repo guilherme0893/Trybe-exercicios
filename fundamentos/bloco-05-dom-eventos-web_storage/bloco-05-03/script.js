47779442591ae1dfd49c44098564317f0f9737fd
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-09-08 18:58:34 -0300

    adiçao das resoluçoes

--- fundamentos/bloco-05-dom-eventos-web_storage/bloco-05-03/script.js
+++ fundamentos/bloco-05-dom-eventos-web_storage/bloco-05-03/script.js
@@ -1,5 +1,5 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sáado']
   const weekDaysList = document.querySelector('.week-days');
 
   for (let index = 0; index < weekDays.length; index += 1) {
@@ -14,25 +14,40 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function createDaysOfTheMonth () {
-  let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
-  let diasDoMes = document.querySelector('#days');
+
+let diasDoMes = document.querySelector('#days');
+
   for (let index = 0; index < dezDaysList.length; index +=1) {
     let dia = dezDaysList[index];
-    // o id days está inserido em uma UL, logo precisará dos Li
+   //  o id days está inserido em uma UL, logo precisará dos Li
     let diaLista = document.createElement('li');
 
-    if (dia === 24 && dia === 25 && dia === 31) {
+    if (dia === 24 | dia === 31) {
       diaLista.className = 'holiday';
+      diaLista.innerHTML = dia;
       diasDoMes.appendChild(diaLista);
-    } else if (dia === 4 && dia === 11 && dia === 18 && dia === 25) {
+    } else if (dia === 4 | dia === 11 | dia === 18) {
       diaLista.className = 'friday';
+      diaLista.innerHTML = dia;
+      diasDoMes.appendChild(diaLista);
+    } else if (dia === 25) {
+      diaLista.className = 'day holiday friday'
+      diasDoMes.innerHTML = dia;
       diasDoMes.appendChild(diaLista);
     } else {
+      diaLista.innerHTML = dia;
       diaLista.className = 'day';
       diasDoMes.appendChild(diaLista)
     }
   };
 };
-createDaysOfTheMonth ()
+ createDaysOfTheMonth ();
+
+// exercicio 2
+
+function criadorDeFeriado (Feriados) {
+  let feriado = document.querySelector('.buttons-container');
+}
