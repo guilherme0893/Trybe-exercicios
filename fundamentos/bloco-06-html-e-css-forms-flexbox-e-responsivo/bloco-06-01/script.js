a6fde0f0d63711ae4616ad2b68cf0c6dca5dfd66
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-09-17 18:32:11 -0300

    exercicio do form

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/bloco-06-01/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/bloco-06-01/script.js
@@ -8,9 +8,13 @@ function chooseState() {
     // add o lenght de opções para o option
     // talvez o innettext nao precisa, apenas o value -- isso aqui no momento
     states.appendChild(option).innerText = stateOptions[index];
-    // states.appendChild(option).value = stateOptions[index];
+    states.appendChild(option).value = stateOptions[index];
     }
 }
+
+window.onload = function () {
+    chooseState();
+  }
 
 //lembrar que essas opções -- no caos, o option -- só aparece conforme a página é carregada 
 // o mesmo vale para os botões que só funcionam após o carregamento
@@ -19,23 +23,33 @@ function chooseState() {
 window.onload = function() {
     chooseState();
     let submitButton = document.querySelector('.submit-button');
-    submitButton.addEventListener('click', submit);
+    submitButton.addEventListener('click', handleSubmit);
 
     let clearButton = document.querySelector('.clear-button');
-    clearButton.addEventListener('click', clear);
+    clearButton.addEventListener('click', clearFields);
 }
 
 // agora usar o preventDefault para bloquear o funcionamento padrão do botão e não carregar a pagina
-function submit(event) {
+function handleSubmit(event) {
     event.preventDefault();
     // para validar a data chama-se uma funçao validateData --- a seguir
     let validation = validateData();
 
     clearDivs();
+
+    if (validation.erroQtd === 0) {
+        renderData();
+    } else {
+        // unshift adiciona add elemento ao começo do array e retorna o novo length
+        validation.messages.unshift('Dados Inválidos')
+
+        renderErrorMessages(validation.messages)
+    }
 }
 
 // data é de dado, não de data de dia/mes/ano
 // essa function vai focar na validação dos dados inseridos no form
+
 function validateData() {
     let validationList = {}
     // aqui é interessante porque ele vai atrás de um objeto chamado inputs onde estao alguns dados dos inputs
@@ -63,6 +77,10 @@ function validateData() {
     }
 };
 
+// criar uma função que vai checar as datas e os anos 
+// validando começaria como false e apois a checagem ficaria como true
+// o conceito de validate seria uma rotina para validar os dados que entram no input
+
 function validateInput(inputName) {
     // cria variavel que pegar o na
     let inputType = inputs[inputName].type
@@ -85,6 +103,133 @@ let validationStrategies = {
     select: selectValidation,
     radio : radioValidation,
 }
+
+function defaultValidation(input, name) {
+    let trimmed = input.value.trim();
+    let validation = inputs[name];
+
+    if (validation.required && trimmed.length === 0) {
+        return false;
+    }
+
+    if (validation.maxLength && trimmed.length > validation.maxLength) {
+        return false;
+    }
+
+    return true;
+}
+
+// acho que é melhor segregar essa function
+
+function validateDay() {
+  if ()
+}
+function dateValidation(input, name){
+  if(input.value.length === 0){
+    return {
+      message: 'A data não foi preenchida!'
+    }
+  }
+  
+  let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
+    
+  if(!regex.test(input.value)){
+    return {
+      message: 'Data: Formato inválido'
+    };
+  }
+  
+  let splitted = input.value.split('/');
+  let day = splitted[0];
+  let month = splitted[1];
+  let year = splitted[2];
+  
+  if(day < 0 || day > 30){
+    return {
+      message: 'Dia inválido'
+    };
+  }
+  
+  if(month < 0 || month > 12){
+    return {
+      message: 'Mês inválido'
+    }
+  }
+  
+  if(year < 0) {
+    return {
+      message: 'Ano inválido'
+    };
+  }
+  
+  return true;
+}
+
+function getSelectedOption(select){
+  return select.options[select.selectedIndex];
+}
+  
+function selectValidation(select, name){
+  let option = getSelectedOption(select);
+  let validation = inputs[name];
+  
+  if(validation.required && (!option || option.disabled)){
+    return false;
+  }
+  
+  return true;
+}
+
+function radioValidation(radio, name){
+  let checked = document.querySelector(`[name=${name}]:checked`)
+    
+  if(checked === null){
+    return false;
+  }
+  
+  return true;
+}
+
+let counter = 0;
+let messages = [];
+
+for(let index in validationsList){
+  if(validationsList[index] === false){
+    counter += 1;
+  }
+
+  if(validationsList[index].message){
+    counter += 1;
+    messages.push(validationsList[index].message);
+  }
+}
+
+return {
+  errorQtd: counter,
+  messages,
+}
+
+function clearDivs(){
+  let errorDivs = document.querySelectorAll('.error');
+  
+  for(div of errorDivs){
+    div.remove();
+  }
+  
+  let dataDiv = document.querySelector('.data');
+  
+  if(dataDiv){
+    dataDiv.remove();
+  }
+ }
+
+  // if(validation.errorQtd === 0){
+  //   renderData();
+  // } else {
+  //   validation.messages.unshift('Dados Inválidos')
+
+  //   renderErrorMessages(validation.messages)
+  // }
 
 let inputs = {
     name: {
