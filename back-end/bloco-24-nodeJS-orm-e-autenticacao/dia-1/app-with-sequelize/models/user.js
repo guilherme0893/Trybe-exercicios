b83e04683476d6a82aa62af2fa9db575f6a98b08
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-07 15:07:22 -0300

    Exercicios back end dia bloco 24 dia1

---
+++ back-end/bloco-24-nodeJS-orm-e-autenticacao/dia-1/app-with-sequelize/models/user.js
@@ -0,0 +1,16 @@
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone_num: DataTypes.STRING,
+  });
+
+  return User;
+};
+  
+module.exports = User;
