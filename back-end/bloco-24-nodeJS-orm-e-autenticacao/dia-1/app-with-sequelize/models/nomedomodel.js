b229eee6163f237d2afdca25ca45940b7d696d15
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-05-04 18:16:47 -0300

    Update

--- back-end/bloco-24-nodeJS-orm-e-autenticacao/dia-1/app-with-sequelize/models/nomedomodel.js
+++ back-end/bloco-24-nodeJS-orm-e-autenticacao/dia-1/app-with-sequelize/models/nomedomodel.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class NomeDoModel extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,11 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    fullName: DataTypes.STRING
+  NomeDoModel.init({
+    nomeDoAtributo: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'NomeDoModel',
   });
-  return User;
+  return NomeDoModel;
 };
